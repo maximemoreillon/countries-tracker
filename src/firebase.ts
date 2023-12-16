7193a0fa4bf155f35769a263d2b3ffc135ca445f
Maxime Moreillon <m.moreillon@gmail.com>
2023-12-16 18:51:21 +0900

    init

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCv0JfyyMpxvlOCHhISeKIGoNcyx_ELqDg",
+  authDomain: "global-friends-countries.firebaseapp.com",
+  projectId: "global-friends-countries",
+  storageBucket: "global-friends-countries.appspot.com",
+  messagingSenderId: "1023646948861",
+  appId: "1:1023646948861:web:407fbb5f910f8f8a6acd94",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
