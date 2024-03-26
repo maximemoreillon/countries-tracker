99503f6ee3fc14b2506cfdb22c35b97f48a2b978
Maxime Moreillon <m.moreillon@gmail.com>
2024-03-26 09:52:33 +0900

    footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import "$lib/firebase"
   import { getAuth, signInAnonymously } from "firebase/auth"
+  import { Icon } from "@smui/button"
 
   const auth = getAuth()
   signInAnonymously(auth)
@@ -16,10 +17,30 @@
 
 <main>
   <slot />
+  <footer>
+    <a href="https://github.com/maximemoreillon/countries-tracker">
+      Countries tracker - Maxime Moreillon
+    </a>
+  </footer>
 </main>
 
 <style>
   :global(body) {
     margin: 0;
+  }
+
+  footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 0.5em;
+    display: flex;
+    justify-content: center;
+  }
+
+  footer a {
+    color: white;
+    text-decoration: none;
   }
 </style>
